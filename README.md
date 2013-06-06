55a389cfd9f06873e010abd6102575a3a94c0492
Andrew <jathom5@andrew-laptop>
2013-06-06 14:03:24 -0500

    Initial commit of files. Added README.md

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 jack-trigger
 ============
+
+A jack client app that listenes on an audio port for transients.  When a threshold is crossed, a midi event is sent on a (jack) midi output port.
+
+This is very useful for things like triggering a kick drum in Hydrogen from an audio signal in Ardour, for instance.
+
+To Compile:
+
+    gcc -o trigger jacktrigger.c `pkg-config --cflags --libs jack`
+
+To Run:
+
+    ./trigger KickTrigger 29 30 0.5
---
+++ jacktrigger.c
@@ -0,0 +1,102 @@
+/*
+    Copyright (C) 2004 Ian Esten
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include <jack/jack.h>
+#include <jack/midiport.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+jack_client_t *client;
+jack_port_t *output_port;
+jack_port_t *input_port;
+
+int note_frq;
+int gate;
+int wait_cnt = 0;
+jack_default_audio_sample_t threshold = 0.5f;
+
+void usage()
+{
+	fprintf(stderr, "usage: jack_trigger name note gate_samples threshold\n"); 
+	fprintf(stderr, "eg: jack_trigger KickTrigger 29 30 0.5 \n");
+	fprintf(stderr, "will play an f3 note on input's greater than 0.5 with a 30 sample-length gate in between\n");
+}
+
+int process(jack_nframes_t nframes, void *arg)
+{
+    jack_default_audio_sample_t *in = (jack_default_audio_sample_t *) jack_port_get_buffer(input_port, nframes);
+	void* port_buf = jack_port_get_buffer(output_port, nframes);
+	unsigned char* buffer;
+	jack_midi_clear_buffer(port_buf);
+
+	int i;
+    for(i=0; i<(sizeof (in) /sizeof (in[0]));i++) {
+        if(in[i] > threshold && wait_cnt == 0) {
+            wait_cnt = gate;
+
+            printf("\n%1f\n",in[i]);
+            buffer = jack_midi_event_reserve(port_buf, 1, 3);
+            buffer[2] = 50;
+            buffer[1] = note_frq;
+            buffer[0] = 0x90;
+        }
+        if(wait_cnt > 0) {
+            wait_cnt = --wait_cnt;
+            //printf("Waiting: %d\n",wait_cnt);
+        }
+    }
+
+	return 0;
+}
+
+int main(int narg, char **args)
+{
+	int i;
+	jack_nframes_t nframes;
+	if(narg<5)
+	{
+		usage();
+		exit(1);
+	}
+	if((client = jack_client_open (args[1], JackNullOption, NULL)) == 0)
+	{
+		fprintf (stderr, "jack server not running?\n");
+		return 1;
+	}
+	jack_set_process_callback (client, process, 0);
+	input_port  = jack_port_register (client, "input", JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
+	output_port = jack_port_register (client, "out", JACK_DEFAULT_MIDI_TYPE, JackPortIsOutput, 0);
+
+
+    note_frq = atoi(args[2]);
+    gate = atoi(args[3]);
+    threshold = atof(args[4]);
+
+	if (jack_activate(client))
+	{
+		fprintf (stderr, "cannot activate client");
+		return 1;
+	}
+
+	while (1)
+	{
+		sleep(1);
+	};
+	
+}
